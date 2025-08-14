f268dc65d517b3be3a0266102d6c747ca965f576
kaiserkonok <kaiserkonok@gmail.com>
2025-08-15 00:17:42 +0600

    added longest substring without repeating characters solution

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -1,13 +1,10 @@
 def lengthOfLongestSubstring(s):
 	if len(s) <= 2:
-		print('yo')
 		return len(s) if len(s) <= 1 else 2 if s[0] != s[1] else 1
 
 	max_count = 0
 
 	for j in range(len(s) - 1):
-		print(f"index: {j}")
-		print(s[j:len(s)])
 		visited = []
 		count = 0
 		for k in range(j, len(s)):
